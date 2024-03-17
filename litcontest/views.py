a7b820fd26a991511dec6dc1cb641b97424bfd9b
Rikki Mongoose <alexander.a.teut@gmail.com>
2024-03-17 22:21:57 +0300

    add more views

--- litcontest/views.py
+++ litcontest/views.py
@@ -3,8 +3,9 @@ from django.template import loader
 from django.shortcuts import get_object_or_404, get_list_or_404, render
 from django.views.decorators.http import require_http_methods
 from django.views.decorators.cache import cache_page
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, ListView, DetailView
 from django.views.generic.edit import FormView, CreateView, UpdateView
+from django.contrib.auth.decorators import login_required
 from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse_lazy
 from .models import Contest, Story, Vote
@@ -28,20 +29,39 @@ class SignUpView(CreateView):
     success_url = reverse_lazy("login")
     template_name = "registration/signup.html"
 
+@login_required
 class StoryFormView(FormView):
-    template_name = "textform.html"
+    template_name = "thermophore/story_form.html"
     form_class = StoryForm
     success_url = "/"
 
+@login_required
 class ContestFormView(FormView):
-    template_name = "contest_form.html"
+    template_name = "thermophore/contest_form.html"
     form_class = ContestForm
     success_url = "/"
 
+@login_required
 class ContestCoordinatorFormView(FormView):
-    template_name = "contest_form.html"
+    template_name = "thermophore/contest_form.html"
     form_class = ContestCoordinatorForm
     success_url = "/"
+
+class ContestCreateView(CreateView):
+    model = Contest
+    fields = ["title", "text"]
+
+class ContestListView(ListView):
+    model = Contest
+    template_name = "thermophore/index.html"
+
+class ContestDetailView(DetailView):
+    model = Contest
+    template_name = "thermophore/contest.html"
+
+class ContestUpdateView(UpdateView):
+    model = Story
+    fields = ["title", "text"]
 
 class StoryCreateView(CreateView):
     model = Story
@@ -51,18 +71,14 @@ class StoryUpdateView(UpdateView):
     model = Story
     fields = ["title", "text"]
 
-class ContestCreateView(CreateView):
+class StoryDetailView(DetailView):
     model = Story
-    fields = ["title", "text"]
-
-class ContestUpdateView(UpdateView):
-    model = Story
-    fields = ["title", "text"]
+    template_name = "thermophore/story.html"
 
 @require_http_methods(["POST"])
 def vote(request, story_id):
     story = get_object_or_404(Story, pk=story_id)
-    return render(request, "thermophore/text.html", {"story": story})
+    return render(request, "thermophore/story.html", {"story": story})
 
 @cache_page(60 * 60 * 24 * 365)
 def generate_zip(contest_id, group = None):
@@ -75,7 +91,7 @@ def generate_zip(contest_id, group = None):
     return FileResponse(pack_to_zip({x.title: x.text for x in texts}), as_attachment=True, filename=f"{contest_title}.zip")
 
 class RulesView(TemplateView):
-    template_name="rules.html"
+    template_name="thermophore/rules.html"
 
 class AboutView(TemplateView):
-    template_name="about.html"
+    template_name="thermophore/about.html"
