a63c83f486566f6ff4c126b6c230a66062f1c178
Rikki Mongoose <alexander.a.teut@gmail.com>
2024-03-17 13:48:04 +0300

    add views

--- litcontest/views.py
+++ litcontest/views.py
@@ -1,8 +1,11 @@
 from django.http import HttpResponse
 from django.template import loader
 from django.shortcuts import get_object_or_404, get_list_or_404, render
-from django.viewes.decorators.http import require_http_methods
+from django.views.decorators.http import require_http_methods
+from django.views.generic import TemplateView
+from django.views.generic.edit import FormView, CreateView, UpdateView
 from .models import Contest, Story, Vote
+from .forms import ContestForm, ContestCoordinatorForm, StoryForm
 
 def index(request):
     contests = get_list_or_404(Contest)
@@ -16,7 +19,44 @@ def story(request, story_id):
     story = get_object_or_404(Story, pk=story_id)
     return render(request, "thermophore/text.html", {"story": story})
 
+class StoryFormView(FormView):
+    template_name = "textform.html"
+    form_class = StoryForm
+    success_url = "/"
+
+class ContestFormView(FormView):
+    template_name = "contestform.html"
+    form_class = ContestForm
+    success_url = "/"
+
+class ContestCoordinatorFormView(FormView):
+    template_name = "contestform.html"
+    form_class = ContestCoordinatorForm
+    success_url = "/"
+
+class StoryCreateView(CreateView):
+    model = Story
+    fields = ["title", "text"]
+
+class StoryUpdateView(UpdateView):
+    model = Story
+    fields = ["title", "text"]
+
+class ContestCreateView(CreateView):
+    model = Story
+    fields = ["title", "text"]
+
+class ContestUpdateView(UpdateView):
+    model = Story
+    fields = ["title", "text"]
+
 @require_http_methods(["POST"])
 def vote(request, story_id):
     story = get_object_or_404(Story, pk=story_id)
     return render(request, "thermophore/text.html", {"story": story})
+
+class RulesView(TemplateView):
+    template_name="rules.html"
+
+class AboutView(TemplateView):
+    template_name="about.html"
